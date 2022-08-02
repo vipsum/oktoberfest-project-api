d30513c21898c1b5404121fc5bcc19f06c301eee
vipsum <83631487+vipsum@users.noreply.github.com>
2022-08-02 18:03:26 -0300

    added tent repo

---
+++ src/main/java/net/oktoberfest/repository/TentRepository.java
@@ -0,0 +1,11 @@
+package net.oktoberfest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.oktoberfest.model.entities.Tent;
+
+@Repository
+public interface TentRepository extends JpaRepository<Tent, Long>{
+    
+}
