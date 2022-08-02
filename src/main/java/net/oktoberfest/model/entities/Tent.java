d4020f9b119bbc24feb2cebb686030a9922b1f8f
vipsum <83631487+vipsum@users.noreply.github.com>
2022-08-02 18:03:34 -0300

    added comment

--- src/main/java/net/oktoberfest/model/entities/Tent.java
+++ src/main/java/net/oktoberfest/model/entities/Tent.java
@@ -25,16 +25,21 @@ public class Tent {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "beer_in_tent")
     private BeerJug beerJug;
+
+    //List of people who will be in the reservation list. If ID '1'
+    //is on the list, the person whose that id corresponds to will be able to 
+    //enter the tent. Many people may have reservations, which is why it is a list of people.
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "reserved_tent")
     private List<Person> reservation;
 
-
-    public Tent(){
+    public Tent() {
 
     }
 
-    public Tent(List<Person> currentOccupation, int maxCapacity, boolean music, BeerJug beerJug, List<Person> reservation){
+    public Tent(List<Person> currentOccupation, int maxCapacity, boolean music, BeerJug beerJug,
+            List<Person> reservation) {
         this.maxCapacity = maxCapacity;
         this.currentOccupation = currentOccupation;
         this.music = music;
@@ -47,4 +52,3 @@ public class Tent {
         return "Register success. Tent Id: " + id;
     }
 }
-
